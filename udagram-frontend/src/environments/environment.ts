c9a7b371526317fff63aa6b714f103fe362d9fac
NhungPhan0801 <phanthinhungbee@gmail.com>
2024-09-25 21:12:14 +0700

    proj3 commit screenshots

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://af424c3dcd97a4ddb81afe933397da87-609361651.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
